b03eade2a484aade0dd22657f8ed2e737212355b
slo0ey <rurchi1206@gmail.com>
2024-12-01 07:13:18 +0900

    feat: db 기본 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MysqlConfigProvider } from './db/mysql.provider';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.development.env', '.production.env'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: MysqlConfigProvider,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/mysql.provider.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class MysqlConfigProvider implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get('DB_USERNAME'),
+      password: this.configService.get('DB_PASSWORD'),
+      database: this.configService.get('DB_DATABASE'),
+      timezone: 'Asia/Seoul',
+      charset: 'UTF8MB4_GENERAL_CI',
+      autoLoadEntities: true,
+      synchronize: this.configService.get<boolean>('DB_USESYNC', false),
+      logging: this.configService.get<boolean>('DB_LOGGING', true),
+      retryAttempts: 2,
+    };
+  }
+}
