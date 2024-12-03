7147dfc2cb63fa463379e085ef5ab36abda1fd92
slo0ey <rurchi1206@gmail.com>
2024-12-03 17:10:34 +0900

    fix: Card레포 생성시 발생하던 오류 해결

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CardController } from './card.controller';
 import { CardService } from './card.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BusinessCardEntity } from 'src/db/entities/card.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BusinessCardEntity])],
   controllers: [CardController],
   providers: [CardService],
 })
