dfdc5b38be2a5061f885fbebd619e910af7c06d7
slo0ey <rurchi1206@gmail.com>
2025-03-19 11:07:59 +0900

    express로 변경

---
+++ app.js
@@ -0,0 +1,53 @@
+const dotenv = require('dotenv');
+
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '.env.development' });
+} else {
+  dotenv.config({ path: '.env.production' });
+}
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
+const port = process.env.PORT || '80';
+app.set('port', port);
+
+if (process.env.NODE_ENV === 'development') {
+  const http = require('http');
+  http.createServer(app).listen(port);
+} else {
+  const https = require('https');
+  const fs = require('fs');
+  const options = {}; // TODO: https options
+  https.createServer(options, app).listen(port);
+}
